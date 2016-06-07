e583d0186f1f5582bdc3448b562630cd938027d4
Semenov Alexander <semenov@skeeks.com>
2016-06-07 15:43:08 +0300

    Create ArtificialAsset.php

---
+++ ArtificialAsset.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @author Semenov Alexander <semenov@skeeks.com>
+ * @link http://skeeks.com/
+ * @copyright 2010 SkeekS (СкикС)
+ * @date 25.02.2016
+ */
+namespace skeeks\template\artificial;
+use yii\web\AssetBundle;
+class UnifyAsset extends AssetBundle
+{
+    public $sourcePath = '@skeeks/template/artificial/src/HTML/assets/';
+    /**
+     * @param string $asset
+     * @return string
+     * @throws \yii\base\InvalidConfigException
+     */
+    static public function getAssetUrl($asset)
+    {
+        return \Yii::$app->assetManager->getAssetUrl(\Yii::$app->assetManager->getBundle(static::className()), $asset);
+    }
+}
